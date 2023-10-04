fefe6771783d9b7f4c2a49d0c72b17a93724026c
Qharny <Qharny>
2023-10-04 15:16:32 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'dart-&-Flutter/main'.
    
    Changes to be committed:
    	new file:   Basic of Dart/loop.dart

---
+++ Basic of Dart/loop.dart
@@ -0,0 +1,26 @@
+// A for loop
+
+void main() {
+  for (int i = 1; i <= 5; i++) {
+    print('Iteration $i');
+  }
+
+
+
+// A while loop
+
+  int count = 1;
+  while (count <= 5) {
+    print('Iteration $count');
+    count++;
+  }
+
+
+  // A do while loop
+  int num = 1;
+  do {
+    print('Iteration $num');
+    num++;
+  } while (num <= 5);
+}
+
