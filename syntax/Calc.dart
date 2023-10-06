bfa175c8310584b52e34af40306cc4a714138a6e
Qharny <Qharny>
2023-10-06 09:17:13 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'dart-&-Flutter/main'.
    
    Changes to be committed:
    	modified:   syntax/Calc.dart

--- syntax/Calc.dart
+++ syntax/Calc.dart
@@ -3,28 +3,41 @@
 // first, import the i/o
 import 'dart:io';
 
-// Get 2 numbers
-void main() {
-  print("Enter a number: ");
-  var firstNumber = stdin.readLineSync();
-  print("Enter another number: ");
-  var secondNumber = stdin.readLineSync();
-
-  print("※※※※※※※※※※※※");
-  print(
-      " Select an Operation. \n1. Addition \n2.Division \n3.Multiplication \n4. Subtraction");
-}
 // Functions for the Operations
 int sum(int a, int b) {
   return a + b; // addition function
 }
 
 int subtract(int a, int b) {
-  return a - b;
+  return a - b; // subtracton function
 }
+
 int multiply(int a, int b) {
-  return a * b;
+  return a * b; // multiplication function
 }
+
 double divide(int a, int b) {
-  return a / b;
+  if (b != 0) {
+    return a / b.toDouble();
+  } else {
+    print("Error: Division by zero!");
+    return double.nan; // Return NaN for division by zero error
+  }
+}
+
+// Get 2 numbers
+void main() {
+  print("Enter a number: ");
+  String? firstNumberInput = stdin.readLineSync();
+  int? firstNumber = int.tryParse(firstNumberInput ?? '');
+  print("Enter another number: ");
+  String? secondNumberInput = stdin.readLineSync();
+  int? secondNumber = int.tryParse(secondNumberInput ?? '');
+
+
+  print("※※※※※※※※※※※※");
+  print(
+      " Select an Operation. \n1. Addition \n2.Division \n3.Multiplication \n4. Subtraction");
+  var operation = stdin.readLineSync();
+
 }
